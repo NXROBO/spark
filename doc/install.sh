45b624bf5335e0b41059c75233f7f72de3bbcdaa
xiankaichen <xiankai.chen@qq.com>
2017-06-10 23:07:43 +0800

    remove the libmsc which is invalid

--- doc/install.sh
+++ doc/install.sh
@@ -4,9 +4,6 @@ echo 'Setting udev rules'
 sudo cp ./rules/70-ttyusb.rules /etc/udev/rules.d/
 sudo cp ./rules/orbbec-usb.rules /etc/udev/rules.d/556-orbbec-usb.rules
 
-echo 'Copying libmsc.so to /usr/lib/'
-sudo cp ../spark_tutorials/spark_iflytek/lib/x64/libmsc.so /usr/lib/
-
 echo 'Installing required libs'
 sudo apt-get install ros-indigo-ecl ros-indigo-ecl-threads ros-indigo-rgbd-launch 
 sudo apt-get install ros-indigo-image-common
