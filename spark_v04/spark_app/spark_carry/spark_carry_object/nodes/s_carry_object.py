478e98c858624166c7c921e0bd59c10bf93731f6
xiankai.chen <xiankai.chen@qq.com>
2017-07-18 18:33:31 +0800

    add uarm grasp

--- spark_v04/spark_app/spark_carry/spark_carry_object/nodes/s_carry_object.py
+++ spark_v04/spark_app/spark_carry/spark_carry_object/nodes/s_carry_object.py
@@ -14,10 +14,14 @@ import threading
 import thread
 import string
 import math
+import cv2
 import numpy as np
 from geometry_msgs.msg import Twist
 from std_msgs.msg import String
 from spark_carry_object.srv import *
+from swiftpro.msg import *
+from cv_bridge import CvBridge, CvBridgeError
+from sensor_msgs.msg import Image
 
 from tf import TransformListener
 import tf
@@ -40,15 +44,127 @@ class GraspObject(State):
         '''
         初始化
         '''
+		global sub, pub1, pub2
+		global xc, yc, xc_prev, yc_prev, found_count
         State.__init__(self, outcomes=['succeeded', 'aborted'])
-        self.task = 'grasp object'        
-        
-    def execute(self, userdata):        
+        self.task = 'grasp object'
+		xc = 0
+		yc = 0
+		xc_prev = xc
+		yc_prev = yc
+		found_count = 0
+		rospy.init_node('image_converter', anonymous=True)
+		sub  = rospy.Subscriber("/camera/rgb/image_raw", Image, self.image_callback)
+		pub1 = rospy.Publisher('position_write_topic', position, queue_size=10)
+		pub2 = rospy.Publisher('pump_topic', status, queue_size=1)
+		
+		try:
+			rospy.spin()
+		except KeyboardInterrupt:
+			print("Shutting down")
+		cv2.destroyAllWindows()
+
+	def image_callback(self, data):
+		global xc, yc, xc_prev, yc_prev, found_count
+		# change to opencv
+		try:
+			cv_image1 = CvBridge().imgmsg_to_cv2(data, "bgr8")
+		except CvBridgeError as e:
+			print('error')
+	
+		# change rgb to hsv
+		cv_image2 = cv2.cvtColor(cv_image1, cv2.COLOR_BGR2HSV)
+	
+		# extract blue
+		LowerBlue = np.array([100, 90, 80])
+		UpperBlue = np.array([130, 255, 255])
+		mask = cv2.inRange(cv_image2, LowerBlue, UpperBlue)
+		cv_image3 = cv2.bitwise_and(cv_image2, cv_image2, mask=mask)
+	
+		# gray process
+		cv_image4 = cv_image3[:,:,0]
+	
+		# smooth and clean noise
+		blurred = cv2.blur(cv_image4, (9, 9))
+		(_, thresh) = cv2.threshold(blurred, 90, 255, cv2.THRESH_BINARY)
+		kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (25, 25))
+		cv_image5 = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
+		cv_image5 = cv2.erode(cv_image5, None, iterations=4)
+		cv_image5 = cv2.dilate(cv_image5, None, iterations=4)
+	
+		# detect contour
+		cv2.imshow("win1", cv_image1)
+		cv2.imshow("win2", cv_image5)
+		cv2.waitKey(1)
+		contours, hier = cv2.findContours(cv_image5, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+		# if find contours, pick the biggest box
+		if len(contours) > 0:
+			size = []
+			size_max = 0
+			for i, c in enumerate(contours):
+				rect = cv2.minAreaRect(c)
+				box = cv2.cv.BoxPoints(rect)
+				box = np.int0(box)
+				x_mid = (box[0][0] + box[2][0] + box[1][0] + box[3][0]) / 4
+				y_mid = (box[0][1] + box[2][1] + box[1][1] + box[3][1]) / 4
+				w = math.sqrt((box[0][0] - box[1][0])**2 + (box[0][1] - box[1][1])**2)
+				h = math.sqrt((box[0][0] - box[3][0])**2 + (box[0][1] - box[3][1])**2) 
+				size.append(w * h)
+				if size[i] > size_max:
+					size_max = size[i]
+					index = i
+					xc = x_mid
+					yc = y_mid
+			# if box is not moving for 20 times
+			if found_count >= 20:
+				self.grasp()
+			else:
+				# if box is not moving
+				if abs(xc - xc_prev) <= 2 and abs(yc - yc_prev) <= 2:
+					found_count = found_count + 1
+				else:
+					found_count = 0
+		else:
+			found_count = 0
+		xc_prev = xc
+		yc_prev = yc
+
+
+    def execute(self, userdata):
         '''
         执行状态
         :param userdata:
         '''
-        return 'succeeded'    
+		global sub, pub1, pub2, xc, yc, found_count
+		# stop function
+		sub.unregister()
+		r1 = rospy.Rate(0.05)
+		r2 = rospy.Rate(10)
+		a = [0.0325,   -1.1880,   -0.0000,   -0.0002,    0.0004,  439]
+		b = [-1.1016,  -0.0166,    0.0006,    0.0001,   -0.0003,  327]
+		pos = position()
+		# start pump
+		pub2.publish(1)
+		r2.sleep()
+		# go forward
+		pos.x = a[0] * xc + a[1] * yc + a[2] * xc * yc + a[3] * xc * xc + a[4] * yc * yc + a[5]
+		pos.y = b[0] * xc + b[1] * yc + b[2] * xc * yc + b[3] * xc * xc + b[4] * yc * yc + b[5]
+		pos.z = 20
+		pub1.publish(pos)
+		r2.sleep()
+		# go down
+		pos.z = -50
+		pub1.publish(pos)
+		r2.sleep()
+		# go back home
+		pos.x = 120
+		pos.y = 0
+		pos.z = 35
+		pub1.publish(pos)
+		r1.sleep()
+        return 'succeeded'
+
 
 class ReleaseObject(State):
     '''
@@ -58,15 +174,38 @@ class ReleaseObject(State):
         '''
         初始化
         '''
+		global pub1, pub2
         State.__init__(self, outcomes=['succeeded', 'aborted'])
-        self.task = 'release object'        
+        self.task = 'release object' 
+		rospy.init_node('image_converter', anonymous=True)
+		pub1 = rospy.Publisher('position_write_topic', position, queue_size=10)
+		pub2 = rospy.Publisher('pump_topic', status, queue_size=4)   
         
     def execute(self, userdata):        
         '''
         执行状态
         :param userdata:
         '''
-        return 'succeeded'    
+		global pub1, pub2
+		r1 = rospy.Rate(0.1)		# 5s
+		r2 = rospy.Rate(10)			# 0.1s
+		pos = position()
+		# go forward
+		pos.x = 200
+		pos.y = 0
+		pos.z = 0
+		pub1.publish(pos)
+		r1.sleep()
+		# stop pump
+		pub2.publish(0)
+		r2.sleep()
+		# go back home
+		pos.x = 120
+		pos.y = 0
+		pos.z = 35
+		pub1.publish(pos)
+		r1.sleep()
+        return 'succeeded'
             
 
 # 获得厨房位置  
