84d8cc725d27e592ac11e1aa39eff46e198ed3b5
jie.xiao <jie.xiao@nxrobo.com>
2018-08-01 10:21:57 +0800

    修改脚本因IPV6导致的解释BUG

--- onekey.sh
+++ onekey.sh
@@ -43,15 +43,15 @@ check_sys(){
 
 #安装ROS完整版
 install_ros_full(){
-		sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-		sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
 		sudo sh -c 'echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6'
+		sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+		sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116		
 		sudo apt-get update
 		sudo apt-get install -y ros-${ROS_Ver}-desktop-full
 		sudo rosdep init
 		rosdep update
 		echo "source /opt/ros/${ROS_Ver}/setup.bash" >> ~/.bashrc
-		source ~/.bashrc
+		source /opt/ros/${ROS_Ver}/setup.bash
 }
 
 #安装SPARK依赖库
