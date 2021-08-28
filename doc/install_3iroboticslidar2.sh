3cba9c8cce829e0481c78b78fcfa6c74b577b7dc
iamzhuang <zsxrock@gmail.com>
2021-08-28 14:21:05 +0800

    add install shell scripts for lidar type

---
+++ doc/install_3iroboticslidar2.sh
@@ -0,0 +1,5 @@
+echo '安装杉川雷达'
+sudo cp ~/spark/doc/lidar_3iroboticslidar2.txt /opt/lidar.txt
+echo '安装完成'
+
+
---
+++ doc/install_eai_g2.sh
@@ -0,0 +1,5 @@
+echo '安装EAI G2雷达'
+sudo cp ~/spark/doc/lidar_ydlidar_g2.txt /opt/lidar.txt
+echo '安装完成'
+
+
