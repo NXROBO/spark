185b94995e9c6ccf30a8453ec4e345b8cd09c4fc
iamzhuang <zsxrock@gmail.com>
2021-08-30 15:38:10 +0800

    update the usb rules for eai lidar

--- doc/install_eai_g2.sh
+++ doc/install_eai_g2.sh
@@ -1,5 +1,8 @@
 echo '安装EAI G2雷达'
 sudo cp ~/spark/doc/lidar_ydlidar_g2.txt /opt/lidar.txt
+sudo chmod 755 ~/spark/src/spark_driver/lidar/ydlidar_g2/startup/initenv.sh 
+sudo ~/spark/src/spark_driver/lidar/ydlidar_g2/startup/initenv.sh
 echo '安装完成'
+echo '请重新拔插雷达的USB接口'
 
 
